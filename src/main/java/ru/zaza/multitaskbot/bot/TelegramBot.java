26eb85cf5f7cc90ca638111328d00fe7376d685a
Zaza Pipia <zaza.pipia.023@gmail.com>
2023-05-22 15:41:56 +0300

    changed TelegramBot code

--- src/main/java/ru/zaza/multitaskbot/bot/TelegramBot.java
+++ src/main/java/ru/zaza/multitaskbot/bot/TelegramBot.java
@@ -3,19 +3,20 @@ package ru.zaza.multitaskbot.bot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.zaza.multitaskbot.config.BotConfig;
+import ru.zaza.multitaskbot.handlers.impl.UpdateHandler;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
     private final BotConfig botConfig;
+    private final UpdateHandler updateHandler;
 
     @Autowired
-    public TelegramBot(BotConfig botConfig) {
+    public TelegramBot(BotConfig botConfig, UpdateHandler updateHandler) {
         this.botConfig = botConfig;
+        this.updateHandler = updateHandler;
     }
 
     @Override
@@ -31,23 +32,9 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
 
-        if (update.hasCallbackQuery()) {
-
-        } else {
-            handleMessage(update.getMessage());
+        if (updateHandler.supports(update)) {
+            updateHandler.handle(update);
         }
-
-    }
-
-    public void handleMessage(Message message) {
-        String msgText = message.getText();
-
-        if (msgText.equals("/add_periphery")) {
-
-        }
-    }
-
-    public void handleCallbackQuery(CallbackQuery callbackQuery) {
 
     }
 }
