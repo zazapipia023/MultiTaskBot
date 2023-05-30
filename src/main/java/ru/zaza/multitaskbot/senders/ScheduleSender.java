eadc722beb1975b6cf632ce928dc14ac24000b5e
Zaza Pipia <zaza.pipia.023@gmail.com>
2023-05-30 12:00:55 +0300

    add sendMakeReportMessage method

--- src/main/java/ru/zaza/multitaskbot/senders/ScheduleSender.java
+++ src/main/java/ru/zaza/multitaskbot/senders/ScheduleSender.java
@@ -1,6 +1,7 @@
 package ru.zaza.multitaskbot.senders;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.DefaultAbsSender;
@@ -14,6 +15,9 @@ public class ScheduleSender extends DefaultAbsSender {
 
     private final BotConfig botConfig;
 
+    @Value("${task.receiver}")
+    private Long taskReceiver;
+
     @Autowired
     public ScheduleSender(BotConfig botConfig) {
         super(new DefaultBotOptions());
@@ -22,12 +26,17 @@ public class ScheduleSender extends DefaultAbsSender {
 
     @Scheduled(cron = "0 0 8 * * *")
     private void sendFillFridgeMessage() {
-        sendMessage(1817131985L, "Нужно заполнить холодильник, и прислать фотографию в рабочий чат");
+        sendMessage(taskReceiver, "Нужно заполнить холодильник, и прислать фотографию в рабочий чат");
+    }
+
+    @Scheduled(cron = "0 30 9,21 * * *")
+    private void sendMakeReportMessage() {
+        sendMessage(taskReceiver, "Напишите отчет за смену");
     }
 
     @Scheduled(cron = "0 15 10 * * *")
     private void sendCleaningMessage() {
-        sendMessage(1817131985L, "Нужно провести полную уборку зала");
+        sendMessage(taskReceiver, "Нужно провести полную уборку зала");
     }
 
     private void sendMessage(Long chatId, String message) {
